2f9e6a57b87d25c6008a3c377f8cfd2578627466
Kishor <kishor.muruganandham@fssa.freshworks.com>
2022-12-22 19:01:34 +0530

    Fixed Some Bug

--- Assests/Js/gradient.js
+++ Assests/Js/gradient.js
@@ -9,8 +9,6 @@ function generateGradient() {
 
   if (showHide === "radial") {
     document.querySelector(".linear").style.display = "none";
-    // document.querySelectorAll(".linear").forEach(function (el) {
-    //   el.style.display = "none";});
     let color1 = document.getElementById("Color-1").value;
     let color2 = document.getElementById("Color-2").value;
 
@@ -20,17 +18,15 @@ function generateGradient() {
     document.getElementById("copyCode").innerHTML = `background: ${gradientColorValue};`;
   } else if (showHide === "linear") {
     document.querySelector(".linear").style.display = "block";
-    // document.querySelectorAll(".linear").forEach(function (el) {
-    //   el.style.display = "block";});
 
     let direction = document.querySelectorAll('input[name="direction"]');
     let color1 = document.getElementById("Color-1").value;
     let color2 = document.getElementById("Color-2").value;
 
-    for (let i = 0; i < direction.length; i++) {
-      if (direction[i].checked) {
-        direction[i].previousElementSibling.firstChild.style.setProperty("--pure-black","#5551ff");
-        let directionValue = direction[i].value;
+    for (let i of direction) {
+      if (i.checked) {
+        i.previousElementSibling.firstChild.style.setProperty("--pure-black","#5551ff");
+        let directionValue = i.value;
 
         let gradientColorValue = `linear-gradient(${directionValue}, ${color1}, ${color2})`;
 
@@ -38,7 +34,7 @@ function generateGradient() {
           gradientColorValue;
         document.getElementById("copyCode").innerHTML = `background: ${gradientColorValue};`;
       } else {
-        direction[i].previousElementSibling.firstChild.style.setProperty("--pure-black","inherit");
+        i.previousElementSibling.firstChild.style.setProperty("--pure-black","inherit");
       }
     }
   }
